ded8b8da607e22e9d56d2c4db835a87c748a61dd
Nielskorn <38701871+Nielskorn@users.noreply.github.com>
2024-11-21 10:19:19 +0100

    full implementation of Fizzbuzz as methode fizzbus

--- src/main/java/org/neuefische/Fizzbuzz.java
+++ src/main/java/org/neuefische/Fizzbuzz.java
@@ -10,5 +10,12 @@ public class Fizzbuzz {
     public static String fizzingAddBuzzing(int n) {
         return (n % 15 == 0) ? "FizzBuzz" : null;
     }
+    public static String fizbus(int n) {
+        if (n % 15 == 0) return "FizzBuzz";
+        else if (n % 5 == 0) return "Buzz";
+        else if (n % 3 == 0) return "Fizz";
+        else return String.valueOf(n);
+
+    }
 
 }
--- src/test/java/FizzbuzzTest.java
+++ src/test/java/FizzbuzzTest.java
@@ -3,22 +3,28 @@ import org.neuefische.Fizzbuzz;
 
 public class FizzbuzzTest {
     @Test
+    void testFizbuss() {
+        String expected = "1";
+        int num=1;
+        assert expected.equals(Fizzbuzz.fizbus(num));
+    }
+    @Test
     void expectFizz__whenGiven_3(){
     //expecet
         String expectResult = "Fizz";
         //given
         int num=3;
-        assert expectResult.equals(Fizzbuzz.fizzing(num));
+        assert expectResult.equals(Fizzbuzz.fizbus(num));
 
     }
     @Test void expectBuzz__whenGiven_5(){
         String expectResult = "Buzz";
         int num=5;
-        assert expectResult.equals(Fizzbuzz.buzzing(num));
+        assert expectResult.equals(Fizzbuzz.fizbus(num));
     }
     @Test void expectFizzbuzz__whenGiven_15(){
         String expectResult = "FizzBuzz";
         int num=15;
-        assert expectResult.equals(Fizzbuzz.fizzingAddBuzzing(num));
+        assert expectResult.equals(Fizzbuzz.fizbus(num));
     }
 }
